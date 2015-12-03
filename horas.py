a18a5158545be14ae0fc50bb49cbdebc49196f7e
Juan Cabrera <jjcp.91@gmail.com>
2015-12-02 22:39:29 -0300

    Horas()._leer_hs() y Horas()._salvar_hs()

--- horas.py
+++ horas.py
@@ -1,7 +1,16 @@
 import os
+import json
 
 
 class Horas(object):
 
     def __init__(self, ruta_hs='~/.horas.json'):
         self.ruta_hs = os.path.expanduser(ruta_hs)
+
+    def _leer_hs(self):
+        return json.load(open(self.ruta_hs, 'r'))
+
+    def _salvar_hs(self, hs):
+        with open(self.ruta_hs, 'w') as f:
+            f.write(json.dumps(hs, indent=4))
+            f.write('\n')
